2b3efc530755ffacffcc39f54b4ac9c9f95fef27
Jim Geovedi <jim@geovedi.com>
2017-02-18 02:02:42 +0700

    added read_vocab

--- phrasematcher.py
+++ phrasematcher.py
@@ -6,17 +6,39 @@ import io
 import re
 import binascii
 from collections import defaultdict
+import logging
+logging.basicConfig(
+    format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
 
 UNK = 0
 
 
 class PhraseMatcher(object):
-    def __init__(self, pattern_file, max_len=10, tokenizer=lambda x: x.split()):
+    def __init__(self, pattern_file, vocab_file=None, max_len=10,
+                 tokenizer=lambda x: x.split()):
         self.tokenizer = tokenizer
-        self._build_vocab(pattern_file)
+        if vocab_file:
+            self._read_vocab(vocab_file)
+        else:
+            self._build_vocab(pattern_file)
         self._compile(pattern_file, max_len=max_len)
 
+    def _read_vocab(self, fname):
+        logging.info('Reading vocab file...')
+        vocab = defaultdict(int)
+        vocab['<unk>'] = UNK
+        for line in io.open(fname, 'r', encoding='utf-8'):
+            parts = self.tokenizer(line.lower().strip())
+            if len(parts) == 2:
+                if word not in vocab:
+                    vocab[word] = len(vocab)
+        self.vocab = vocab
+        logging.info('Vocab size: {}'.format(len(self.vocab)))
+
     def _build_vocab(self, fname):
+        logging.info('Start building vocab...')
         vocab = defaultdict(int)
         for line in io.open(fname, 'r', encoding='utf-8'):
             for token in self.tokenizer(line.strip()):
@@ -26,14 +48,19 @@ class PhraseMatcher(object):
                      for k, v in enumerate(reversed([k for k, v in vocab])))
         vocab['<unk>'] = UNK
         self.vocab = vocab
+        logging.info('Vocab size: {}'.format(len(self.vocab)))
 
     def _compile(self, fname, max_len=10):
+        logging.info('Start compiling patterns...')
         self.bos = defaultdict(set)
         self.eos = defaultdict(set)
         self.checksums = defaultdict(set)
         self.lengths = set()
+        n_patterns = 0
 
-        for line in io.open(fname, 'r', encoding='utf-8'):
+        for line_no, line in enumerate(io.open(fname, 'r', encoding='utf-8')):
+            if line_no % 10000 == 0:
+                logging.info('Processing patterns: {}'.format(line_no))
             arr = [self.vocab.get(t.lower(), 0) for t in line.strip().split()]
             if UNK in set(arr):
                 continue
@@ -46,6 +73,8 @@ class PhraseMatcher(object):
             c_idx = (arr_len, arr[0], arr[-1])
             self.checksums[c_arr].add(c_idx)
             self.lengths.add(arr_len)
+            n_patterns += 1
+        logging.info('Patterns: {} (orig: {})'.format(n_patterns, line_no))
 
     def checksum(self, arr):
         return binascii.crc32(str(arr))
@@ -57,7 +86,6 @@ class PhraseMatcher(object):
         candidates = set()
         ranges = set()
 
-        # find markers
         for i, tok_int in enumerate(tok_arr):
             if tok_int == UNK:
                 continue
@@ -77,7 +105,6 @@ class PhraseMatcher(object):
                             ranges.add((i, j))
 
         if remove_subset:
-            # filter out overlapping candidates
             ranges = list(sorted(ranges, reverse=True))
             for (i, j), c_idx in list(candidates):
                 for ii, jj in ranges:
@@ -86,6 +113,5 @@ class PhraseMatcher(object):
                     if ii <= i and j <= jj:
                         candidates.remove(((i, j), c_idx))
 
-        # check candidates
         for (i, j), c_idx in candidates:
             yield tok[i:j]
