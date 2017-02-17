7891042ef77d4e2a781a93a9bb6e970ec31152d7
Jim Geovedi <jim@geovedi.com>
2017-02-18 06:51:55 +0700

    refactoring

--- phrasematcher.py
+++ phrasematcher.py
@@ -2,143 +2,134 @@
 
 from __future__ import unicode_literals
 
+import os
 import io
 import re
-import binascii
-import numpy as np
-from collections import defaultdict
+import pickle
+import vedis
+import xxhash
+
 import logging
 logging.basicConfig(
     format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO)
 
-UNK = 0
-
-
-class StorageArray(object):
-    def __init__(self, dim=(10000, 2), dtype='int32'):
-        self.size = 0
-        self.dim = dim
-        self.capacity = dim[0]
-        self.dtype = dtype
-        self.data = np.zeros(self.dim, dtype=self.dtype)
-        self.ndim = np.ndim(self.data)
-
-    def update(self, rows):
-        assert np.ndim(rows) == self.ndim, 'dimension mismatch'
-        for row in rows:
-            self.add(row)
-
-    def add(self, row):
-        if self.size == self.capacity:
-            self.capacity = self.size + self.capacity
-            self.dim = (self.capacity, ) + self.dim[1:]
-            newdata = np.zeros(self.dim, dtype=self.dtype)
-            newdata[:self.size] = self.data
-            self.data = newdata
-        self.data[self.size] = row
-        self.size += 1
-
-    def frozen(self):
-        self.data = self.data[:self.size]
-
-    def __contains__(self, arr):
-        return bool(np.where(np.all(self.data == arr, axis=1))[0].size)
-
 
 class PhraseMatcher(object):
     def __init__(self,
-                 pattern_file,
+                 model_dir,
+                 pattern_file=None,
                  vocab_file=None,
                  max_len=10,
                  tokenizer=lambda x: x.split()):
         self.tokenizer = tokenizer
-        if vocab_file:
-            self._read_vocab(vocab_file)
+        self.model_dir = model_dir
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        self.vocab = vedis.Vedis('{}/vocab'.format(self.model_dir))
+        self.bos = vedis.Vedis('{}/bos'.format(self.model_dir))
+        self.eos = vedis.Vedis('{}/eos'.format(self.model_dir))
+        self.hashtable = vedis.Vedis('{}/hashtable'.format(self.model_dir))
+
+        if pattern_file:
+            if vocab_file:
+                self._read_vocab(vocab_file)
+            else:
+                self._build_vocab(pattern_file)
+            self._compile(pattern_file, max_len=max_len)
         else:
-            self._build_vocab(pattern_file)
-        self._compile(pattern_file, max_len=max_len)
+            with io.open('{}/lengths'.format(self.model_dir), 'r') as fin:
+                self.lengths = set(map(int, fin.read().split()))
 
     def _read_vocab(self, fname):
         logging.info('Reading vocab file...')
-        vocab = defaultdict(int)
-        vocab['<unk>'] = UNK
-        for line in io.open(fname, 'r', encoding='utf-8'):
+        from collections import defaultdict
+        wc = defaultdict(int)
+        for line in io.open(fname, 'r'):
             parts = self.tokenizer(line.lower().strip())
             word = parts[0]
-            if word not in vocab:
-                vocab[word] = len(vocab)
-        self.vocab = vocab
-        logging.info('Vocab size: {}'.format(len(self.vocab)))
+            if word not in wc:
+                wc[word] = len(wc)
+        n_vocab = len(wc)
+        with self.vocab.transaction():
+            self.vocab.mset(dict(wc))
+        logging.info('Vocab size: {}'.format(n_vocab))
 
     def _build_vocab(self, fname):
         logging.info('Start building vocab...')
-        vocab = defaultdict(int)
-        for line in io.open(fname, 'r', encoding='utf-8'):
+        from collections import defaultdict
+        wc = defaultdict(int)
+        for line in io.open(fname, 'r'):
             for token in self.tokenizer(line.strip()):
-                vocab[token.lower()] += 1
-        vocab = sorted(vocab.items(), key=lambda x: x[1])
-        vocab = dict((v, k + 1)
-                     for k, v in enumerate(reversed([k for k, v in vocab])))
-        vocab['<unk>'] = UNK
-        self.vocab = vocab
-        logging.info('Vocab size: {}'.format(len(self.vocab)))
+                wc[token.lower()] += 1
+        wc = sorted(wc.items(), key=lambda x: x[1])
+        wc = dict((v, k) for k, v in enumerate(reversed([k for k, v in wc])))
+        n_vocab = len(wc)
+        with self.vocab.transaction():
+            self.vocab.mset(dict(wc))
+        logging.info('Vocab size: {}'.format(n_vocab))
 
     def _compile(self, fname, max_len=10):
         logging.info('Start compiling patterns...')
-        self.bos = StorageArray((1000, 2))
-        self.eos = StorageArray((1000, 2))
-        self.checksums = StorageArray((10000, 4))
         self.lengths = set()
 
-        for line_no, line in enumerate(io.open(fname, 'r', encoding='utf-8')):
-            if line_no % 10000 == 0:
-                logging.info('Processing patterns: {}'.format(line_no))
-            arr = [self.vocab.get(t.lower(), 0) for t in line.strip().split()]
-            if UNK in set(arr):
-                continue
-            arr_len = len(arr)
-            if arr_len > max_len:
-                continue
-            self.bos.add([arr_len, arr[0]])
-            self.eos.add([arr_len, arr[-1]])
-            c_arr = self.checksum(arr)
-            c_idx = [arr_len, arr[0], arr[-1], c_arr]
-            self.checksums.add(c_idx)
-            self.lengths.add(arr_len)
+        with self.bos.transaction(), self.eos.transaction(), \
+             self.hashtable.transaction(), self.vocab.transaction():
+            n_patterns = 0
+            for i, line in enumerate(io.open(fname, 'r', encoding='utf-8')):
+                if i % 10000 == 0:
+                    logging.info('Processing patterns: {}'.format(i))
+                if isinstance(line, unicode):
+                    line = line.encode('utf-8')
+                arr = self.vocab.mget(line.strip().split())
+                if None in set(arr):
+                    continue
+                arr = [int(a) for a in arr]
+                arr_len = len(arr)
+                if arr_len > max_len:
+                    continue
+                self.bos[b'{}:{}'.format(arr_len, arr[0])] = b'1'
+                self.eos[b'{}:{}'.format(arr_len, arr[-1])] = b'1'
+                self.hashtable[self.hash(arr)] = b'1'
+                self.lengths.add(arr_len)
+                n_patterns += 1
 
-        self.checksums.frozen()
-        n_patterns = self.checksums.data.shape[0]
-        logging.info('Patterns: {} (Read: {})'.format(n_patterns, line_no + 1))
+            logging.info('Patterns: {} (Read: {})'.format(n_patterns, i + 1))
+        with io.open('{}/lengths'.format(self.model_dir), 'w') as fout:
+            fout.write(' '.join(str(i) for i in self.lengths))
 
-    def checksum(self, arr):
-        return binascii.crc32(str(arr))
+    def hash(self, arr):
+        s = b':'.join([str(i) for i in arr])
+        return xxhash.xxh32(s).hexdigest()
 
     def match(self, sentence, remove_subset=False):
+        if isinstance(sentence, unicode):
+            sentence = sentence.encode('utf-8')
         tok = self.tokenizer(sentence.strip())
-        tok_arr = [self.vocab.get(t.lower(), 0) for t in tok]
+        with self.vocab.transaction():
+            tok_arr = self.vocab.mget(tok)
         tok_len = len(tok_arr)
         candidates = set()
 
-        for i, tok_int in enumerate(tok_arr):
-            if tok_int == UNK:
-                continue
-            for p_len in self.lengths:
-                b_int = tok_int
-                j = i + p_len
-                if j + 1 <= tok_len:
-                    e_int = tok_arr[j - 1]
-                    p_arr = tok_arr[i:j]
-                    if UNK in set(p_arr):
-                        continue
-                    b_idx = [p_len, b_int]
-                    e_idx = [p_len, e_int]
-                    if b_idx in self.bos and e_idx in self.eos:
-                        c_arr = self.checksum(p_arr)
-                        c_idx = (p_len, b_int, e_int, c_arr)
-                        if c_idx in self.checksums:
-                            candidates.add((i, j))
+        with self.bos.transaction(), self.eos.transaction(), \
+             self.hashtable.transaction():
+            for i, tok_int in enumerate(tok_arr):
+                if tok_int == None:
+                    continue
+                for p_len in self.lengths:
+                    b_int = int(tok_int)
+                    j = i + p_len
+                    if j + 1 <= tok_len:
+                        p_arr = tok_arr[i:j]
+                        if None in set(p_arr):
+                            continue
+                        e_int = int(tok_arr[j - 1])
+                        b_idx = b'{}:{}'.format(p_len, b_int)
+                        e_idx = b'{}:{}'.format(p_len, e_int)
+                        if b_idx in self.bos and e_idx in self.eos:
+                            if self.hash(p_arr) in self.hashtable:
+                                candidates.add((i, j))
 
         if remove_subset:
             ranges = list(sorted(candidates, reverse=True))
